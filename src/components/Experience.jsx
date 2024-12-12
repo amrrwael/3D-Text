c8aac2e6188d46e326a761da3d0dd69fb00d2053
Amr Hussein <amrwael6j@gmail.com>
2024-12-13 02:54:50 +0700

    Add donuts and optmaize

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,16 +1,21 @@
 import { useMatcapTexture, Center, Text3D, OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
+import { useState } from "react";
 
 export const Experience = () => {
+
+
+  const [ torusGeometry, setTorusGeometry] = useState()
   const [matcapTexture] = useMatcapTexture('5E5855_C6C4CD_C89B67_8F8E98', 256);
 
-  if (!matcapTexture) return null;
 
   return (
     <>
       <OrbitControls makeDefault />
       <Perf position="top-left" />
 
+      <torusGeometry ref={setTorusGeometry}/>
+      
       <Center>
         <Text3D
           font={"./fonts/Merriweather_Sans_Italic.json"}
@@ -28,10 +33,26 @@ export const Experience = () => {
         </Text3D>
       </Center>
 
-      <mesh>
-        <torusGeometry />
+    {[...Array(100)].map((value, index) => 
+    <mesh
+        key={index}
+        geometry={torusGeometry}
+        position={[
+            (Math.random() - 0.5) * 10,
+            (Math.random() - 0.5) * 10,
+            (Math.random() - 0.5) * 10
+        ]}
+        scale={ 0.2 + Math.random() * 0.2}
+        rotation={[
+            Math.random() * Math.PI,
+            Math.random() * Math.PI,
+            0
+        ]}
+        >
         <meshMatcapMaterial matcap={matcapTexture} />
-      </mesh>
+    </mesh>
+    )}
+      
     </>
   );
 };
