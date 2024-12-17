1dcde0dd738ecdc2c6f4234710aa8c1ce6f0c0a5
Amr Hussein <amrwael6j@gmail.com>
2024-12-17 19:52:45 +0700

    Optimize shaders

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,23 +1,48 @@
 import { useMatcapTexture, Center, Text3D, OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
-import { useState } from "react";
+import { useRef, useEffect } from "react"
+import { useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
+
+
+const torusGeometry = new THREE.TorusGeometry
+const material = new THREE.MeshMatcapMaterial
 
 export const Experience = () => {
 
 
-  const [ torusGeometry, setTorusGeometry] = useState()
+  const donutsGroup = useRef()
+
   const [matcapTexture] = useMatcapTexture('5E5855_C6C4CD_C89B67_8F8E98', 256);
 
+  useEffect(() =>
+  {
+    matcapTexture.encoding = THREE.sRGBEncoding
+    matcapTexture.needsUpdate = true
+
+    material.matcap = matcapTexture
+    material.needsUpdate = true
+  }, [])
+
+  useFrame((state, delta) => {
+    for(const donut of donutsGroup.current.children)
+    {
+      donut.rotation.y += delta * 0.2
+    }
+  })
 
   return (
     <>
       <OrbitControls makeDefault />
       <Perf position="top-left" />
 
-      <torusGeometry ref={setTorusGeometry}/>
+      {/* <torusGeometry ref={setTorusGeometry}/>
+      <meshMatcapMaterial ref={SetMaterial} matcap={matcapTexture} /> */}
+
       
       <Center>
         <Text3D
+        material={ material }
           font={"./fonts/Merriweather_Sans_Italic.json"}
           size={0.75}
           height={0.2}
@@ -28,15 +53,16 @@ export const Experience = () => {
           bevelOffset={0}
           bevelSegments={5}
         >
-          This is
-          <meshMatcapMaterial matcap={matcapTexture} />
+          Hello World
         </Text3D>
       </Center>
 
+  <group ref={ donutsGroup }>
     {[...Array(100)].map((value, index) => 
     <mesh
         key={index}
         geometry={torusGeometry}
+        material={ material }
         position={[
             (Math.random() - 0.5) * 10,
             (Math.random() - 0.5) * 10,
@@ -48,11 +74,10 @@ export const Experience = () => {
             Math.random() * Math.PI,
             0
         ]}
-        >
-        <meshMatcapMaterial matcap={matcapTexture} />
-    </mesh>
+        />
+    
     )}
-      
+      </group>
     </>
   );
 };
